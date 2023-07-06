5d730bb000a7367dc590b3f4c1e0278e20da95f1
semfim01 <wobarret@gmail.com>
2023-07-06 15:29:21 -0300

    Update grafica.py

--- ferramentagrafica/grafica.py
+++ ferramentagrafica/grafica.py
@@ -11,3 +11,4 @@ def google():
 
 mygoogle = Button(root, text='Abrir o Google', command = google).pack(pady=20)
 root.mainloop()
+# basicamente vai abrir uma janela na qual quando click o botão abre o navegador no site de busca do google
