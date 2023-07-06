be05f58a41a5eb8ac8c6190a0e995f6970f05024
semfim01 <wobarret@gmail.com>
2023-07-06 14:55:59 -0300

    Update phone.py
    amem conseguir instalar tudo graças a ajuda do chat gpt

--- phonenumbers/phone.py
+++ phonenumbers/phone.py
@@ -6,4 +6,4 @@ phone = input('digite o telefone no formato: +551140028922: ')
 
 phone_number = phonenumbers.parse(phone)
 
-print(geocoder.description_for_nunber(phone_number, 'pt'))
+print(geocoder.description_for_number(phone_number, 'pt'))
